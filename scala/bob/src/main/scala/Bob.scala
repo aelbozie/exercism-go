b4be1f99b5584c14fd598b3bb3b9c6c4b51d0529
Ahmed <ahmed.elbozie@gmail.com>
2019-08-06 07:23:48 +0100

    adding Bob exercise

---
+++ scala/bob/src/main/scala/Bob.scala
@@ -0,0 +1,19 @@
+
+object Bob {
+  def response(statement: String): String = {
+    statement.trim match {
+      case s if s.isEmpty =>
+        "Fine. Be that way!"
+      case s if s.toUpperCase == s && hasLetters(s) && s.endsWith("?") =>
+        "Calm down, I know what I'm doing!"
+      case s if s.endsWith("?") =>
+        "Sure."
+      case s if s.toUpperCase == s && hasLetters(s) =>
+        "Whoa, chill out!"
+      case _ =>
+        "Whatever."
+    }
+  }
+  def hasLetters(statement: String): Boolean =
+    statement.toLowerCase.exists(('a' to 'z').toSet.contains(_))
+}
