5f66ffdd4262bb0d052131b9a58e6540d88d760c
Ahmed <ahmed.elbozie@gmail.com>
2019-08-08 22:27:06 +0100

    defining functions for pattern matching

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -4,16 +4,29 @@ object Bob {
     statement.trim match {
       case s if s.isEmpty =>
         "Fine. Be that way!"
-      case s if s.toUpperCase == s && hasLetters(s) && s.endsWith("?") =>
+      case s if isShoutingQuestion(s) =>
         "Calm down, I know what I'm doing!"
-      case s if s.endsWith("?") =>
+      case s if isQuestion(s) =>
         "Sure."
-      case s if s.toUpperCase == s && hasLetters(s) =>
+      case s if isShouting(s) =>
         "Whoa, chill out!"
       case _ =>
         "Whatever."
     }
   }
+
+  def isShoutingQuestion(statement: String): Boolean =
+    statement.toUpperCase == statement &&
+  hasLetters(statement) &&
+  isQuestion(statement)
+
+  def isShouting(statement: String): Boolean =
+    statement.toUpperCase == statement &&
+  hasLetters(statement)
+
+  def isQuestion(statement: String): Boolean =
+    statement.endsWith("?")
+
   def hasLetters(statement: String): Boolean =
     statement.toLowerCase.exists(('a' to 'z').contains(_))
 }
