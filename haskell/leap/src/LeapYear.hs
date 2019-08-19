b2fae25b4fe15b6ed12fd81375541eca072c6f30
Ahmed <ahmed.elbozie@gmail.com>
2019-08-19 07:50:43 +0100

    using improved guard syntax

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -2,6 +2,7 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year
-  | year `rem` 4 == 0 && year `rem` 100 /= 0 = True
+  | year `rem` 4 == 0
+  , year `rem` 100 /= 0 = True
   | year `rem` 400 == 0 = True
   | otherwise = False
