cb84e89699b8b9d84173cd4d13d6b3eebab1eea9
Ahmed <ahmed.elbozie@gmail.com>
2019-08-19 07:47:49 +0100

    removing columns from init

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -2,10 +2,11 @@ class Matrix(object):
     def __init__(self, matrix_string):
         self.rows = [list(map(int, row.split(' ')))
                      for row in  matrix_string.split('\n')]
-        self.columns = [list(column) for column in zip(*self.rows)]
         
     def row(self, index):
         return self.rows[index - 1]
 
     def column(self, index):
+        self.columns = [list(column) for column in zip(*self.rows)]
         return self.columns[index - 1]
+
