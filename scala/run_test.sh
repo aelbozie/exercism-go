f0ff3e3b2848c81d167437842ccac7fae65b2724
Ahmed <36151122+aelbozie@users.noreply.github.com>
2020-01-27 15:42:38 +0000

    Update run_test.sh
    adding pipe-failing to stop intermediate test fails from giving overall pass

--- scala/run_test.sh
+++ scala/run_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -eo pipefail
 
 for folder in $(ls -d */);
 do  
-cd $folder;
+cd "$folder";
 echo -e "\e[1;32m==========testing $file==========";
 sbt ++$TRAVIS_SCALA_VERSION test;
 cd ..  ;
