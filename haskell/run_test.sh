f6e90bfbf861a6691799cc1cf798b0e4f49a6b46
Ahmed <36151122+aelbozie@users.noreply.github.com>
2020-01-27 18:52:32 +0000

    Update run_test.sh

--- haskell/run_test.sh
+++ haskell/run_test.sh
@@ -1,7 +1,10 @@
-for file in $(ls -d */);
+#!/bin/bash
+set -eo pipefail
+
+for folder in $(ls -d -- */);
 do  
-cd $file;
-echo -e "\e[1;32m==========testing $file==========";
-stack  test;
+cd "$folder";
+echo -e "\e[1;32m==========testing $folder==========";
+stack test;
 cd ..  ;
 done
