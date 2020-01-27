690771df3927d3525b4db5ce407487d1de819951
Ahmed <36151122+aelbozie@users.noreply.github.com>
2020-01-27 19:33:22 +0000

    Update matrix.py

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -2,7 +2,7 @@ class Matrix(object):
     def __init__(self, matrix_string):
         self.rows = [list(map(int, row.split(' ')))
                      for row in  matrix_string.split('\n')]
-        
+
     def row(self, index):
         return self.rows[index - 1]
 
