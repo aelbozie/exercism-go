6018f052aff4e51e13f30ea57e896be58ffe22f8
Ahmed <36151122+aelbozie@users.noreply.github.com>
2020-01-27 18:52:03 +0000

    Update run_test.sh

--- scala/run_test.sh
+++ scala/run_test.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 for folder in $(ls -d -- */);
 do  
 cd "$folder";
-echo -e "\e[1;32m==========testing $file==========";
+echo -e "\e[1;32m==========testing $folder==========";
 sbt ++$TRAVIS_SCALA_VERSION test;
 cd ..  ;
 done
