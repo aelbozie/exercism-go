01886893838ab912cb402ef3ef1dc5275a510c16
aelbozie <36151122+aelbozie@users.noreply.github.com>
2021-04-12 23:22:15 +0100

    feat: pangram haskell

---
+++ haskell/pangram/src/Pangram.hs
@@ -0,0 +1,8 @@
+module Pangram (isPangram) where
+import qualified Data.Char as C (toLower)
+import qualified Data.Set  as S (fromList, isSubsetOf)
+
+isPangram :: String -> Bool
+isPangram text =  alphabet `S.isSubsetOf` set
+  where set =  S.fromList [C.toLower t | t <- text]
+        alphabet = S.fromList "abcdefghijklmnopqrstuvwxyz"
