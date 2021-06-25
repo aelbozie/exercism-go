2114ebad9b611e1f7653b99c09e51e4b5ce74424
aelbozie <36151122+aelbozie@users.noreply.github.com>
2021-06-25 03:06:58 +0100

    feat: bob haskell

---
+++ haskell/bob/src/Bob.hs
@@ -0,0 +1,20 @@
+module Bob (responseFor) where
+
+import qualified Data.Text as T
+import qualified Data.Char as C
+import           Data.Text (Text)
+
+
+responseFor :: Text -> Text
+responseFor text
+    | asking, yelling, hasLetters = T.pack "Calm down, I know what I'm doing!"
+    | asking = T.pack "Sure."
+    | yelling, hasLetters = T.pack "Whoa, chill out!"
+    | empty = T.pack "Fine. Be that way!"
+    | otherwise  = T.pack "Whatever."
+        where
+            strippedText = T.strip text
+            hasLetters = any C.isLetter (T.unpack strippedText)
+            asking = T.isSuffixOf (T.pack "?") strippedText
+            empty = all C.isSpace (T.unpack strippedText)
+            yelling = text == T.toUpper strippedText
