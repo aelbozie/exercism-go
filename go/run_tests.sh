b5d8899ee5baa53364e125b56c46974edd1b2191
aelbozie <36151122+aelbozie@users.noreply.github.com>
2021-07-05 02:46:27 +0100

    fix: improve go coverage reporting

--- go/run_tests.sh
+++ go/run_tests.sh
@@ -7,6 +7,6 @@ cd "$folder";
 echo -e "\e[1;32m==========testing $folder==========\e[0;0m";
 go test -v --bench . --benchmem;
 go test -coverprofile=cover.out;
-bash <(curl -Ls https://coverage.codacy.com/get.sh) report --force-coverage-parser go -r cover.out;
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report --force-coverage-parser go --partial -r cover.out;
 cd ..  ;
 done
