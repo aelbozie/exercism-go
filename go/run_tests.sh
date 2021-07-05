4401a5ef8d0fae9be9e1554a8cbea270e6465f55
aelbozie <36151122+aelbozie@users.noreply.github.com>
2021-07-05 01:57:07 +0100

    feat: go two_fer

--- go/run_tests.sh
+++ go/run_tests.sh
@@ -6,5 +6,7 @@ do
 cd "$folder";
 echo -e "\e[1;32m==========testing $folder==========\e[0;0m";
 go test -v --bench . --benchmem;
+go test -coverprofile=cover.out;
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report --force-coverage-parser go -r cover.out;
 cd ..  ;
 done
