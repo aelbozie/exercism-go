31aab57e2d07d749ba782480ccfeb327251ed102
aelbozie <36151122+aelbozie@users.noreply.github.com>
2021-07-05 02:59:56 +0100

    fix: improve python coverage reporting

--- python/run_tests.sh
+++ python/run_tests.sh
@@ -3,11 +3,13 @@ set -eo pipefail
 
 pip install pytest-cov=="2.12.1"
 
-for folder in $(ls -d -- */);
+for folder in $(ls -d -- */ | sed "s,/$,," );
 do
 cd "$folder";
 echo -e "\e[1;32m==========testing $folder==========\e[0;0m";
-pytest --cov="$folder" --cov-report xml
+
+pyproj=$(echo $folder | sed "s,-,_," )
+pytest --cov="$pyproj" --cov-report xml
 bash <(curl -Ls https://coverage.codacy.com/get.sh) report --partial -r coverage.xml;
 cd ..  ;
 done
