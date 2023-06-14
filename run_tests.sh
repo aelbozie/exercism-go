c8c039a79f2512c41e0e2b850b908bf3b9408cce
Ahmed <36151122+aelbozie@users.noreply.github.com>
2023-06-14 15:53:19 +0100

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -9,9 +9,8 @@ do
     echo -e "\e[1;32m==========testing $dir==========\e[0;0m";
     go test -v --bench . --benchmem;
     go test -coverprofile=cover.out;
-    CODACY_PROJECT_TOKEN=$1
     bash <(curl -Ls https://coverage.codacy.com/get.sh) report --partial --force-coverage-parser go -r cover.out;
     cd ..
 done
-
+CODACY_PROJECT_TOKEN=$1
 bash <(curl -Ls https://coverage.codacy.com/get.sh) final;
